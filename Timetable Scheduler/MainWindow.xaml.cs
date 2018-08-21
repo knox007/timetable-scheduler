1ac31ae7ef92eac989e09b9843be8d1a54d80615
Nguyễn Thành <thanhnguyen2187@gmail.com>
2018-08-21 20:54:24 +0700

    First commit?

--- Timetable Scheduler/MainWindow.xaml.cs
+++ Timetable Scheduler/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Timetable_Scheduler
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
