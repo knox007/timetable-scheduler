ef954f8a56f48b9dd67242ca2c755b54b44d1ff3
Nguyễn Thành <thanhnguyen2187@gmail.com>
2018-09-13 22:09:10 +0700

    Simple changes.

---
+++ Timetable Scheduler/Functions/Data_Crawler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable_Scheduler.Functions
+{
+    class Data_Crawler
+    {
+    }
+}
